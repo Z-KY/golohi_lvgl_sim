3b252b305cef4f1be8c03118c80374102f2595e9
zky <1578479464@qq.com>
2026-09-05 14:24:50 +0800

    Comment lv_demo_widgets

--- src/main.c
+++ src/main.c
@@ -69,7 +69,7 @@ int main(int argc, char **argv)
   /* - lv_demo_stress(); */
   /* - lv_example_label_1(); */
   /* - etc. */
-  lv_demo_widgets();
+  // lv_demo_widgets();
 
   while(1) {
     /* Periodically call the lv_task handler.
